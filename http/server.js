c8fb4913abf2472eb074806b98281f5982a97b37
Pedro Lagares <pedrolagaresmiranda@gmail.com>
2022-02-21 20:34:16 -0300

    front-end

--- http/server.js
+++ http/server.js
@@ -3,15 +3,19 @@ const fs = require('fs')
 const path = require('path')
 
 http.createServer((req, res) => { //req = pedido, res = resposta
-    const file = req
+    const file = req.url === '/' ? 'index.html' : req.url
+    const filePath = path.join(__dirname, 'public' , file)
+    const extname = path.extname(filePath)
 
-    // if ( req.url === '/') {            
-    //     fs.readFile(
-    //         path.join(__dirname, 'public' ,'index.html'),
-    //         (err, content) => {
-    //             if (err) throw err
-    //             res.end(content)
-    //         }
-    //     )
-    // }
+    const allowedFileTypes = ['.html', '.css', '.js']
+    const allowed = allowedFileTypes.find(item => item == extname)
+
+    if ( !allowed) return 
+
+    fs.readFile( filePath,
+        (err, content) => {
+            if (err) throw err
+            res.end(content)
+        }
+    )
 }).listen(5000, () => console.log('Server is runnig'))
